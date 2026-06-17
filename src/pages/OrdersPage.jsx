96d6808603602a3182fc6542067fb6b64a63f6b3
khalandar <muhammedkhadeer198@gmail.com>
2026-06-18 00:52:30 +0530

    Route Checkout page

---
+++ src/pages/OrdersPage.jsx
@@ -0,0 +1,9 @@
+export function OrdersPage() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'Roboto, Arial' }}>
+      <h1>Orders</h1>
+      <p>This page is now routed correctly.</p>
+      <a href="/">Return to home</a>
+    </div>
+  )
+}
