869cad47c93d58a13177251b9b572abfe051e7d9
khalandar <muhammedkhadeer198@gmail.com>
2026-07-02 02:09:46 +0530

    generate html for orders page

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage products={products} cart={cart} />} />
-      <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/orders" element={<OrdersPage cart={cart} />} />
       <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
       <Route path='/tracking' element={<TrackingPage />} />
     </Routes>
